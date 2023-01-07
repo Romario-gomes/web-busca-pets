6b17dd1d1d0e4619c4b00c782cf2215bde59ae94
Romario Alves <romariogn10@gmail.com>
2023-01-06 23:25:35 -0300

    Feat(Initial header Layout): add

--- pages/pets/create/index.tsx
+++
@@ -1,5 +0,0 @@
-export default function CreatePet() {
-  return (
-    <h1>Criar Pet</h1>
-  )
-}
---
+++ src/pages/pets/create/index.tsx
@@ -0,0 +1,7 @@
+import style from "./pet.module.scss";
+
+export default function CreatePet() {
+  return (
+    <div className={style.container}></div>
+  )
+}
