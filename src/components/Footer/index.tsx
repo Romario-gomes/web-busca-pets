ce0270753213f103ee07c07054fe495053a64e97
Romario Alves <romariogn10@gmail.com>
2023-01-12 23:47:25 -0300

    Feat(Footer): add footer (incomplete)

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,7 +1,27 @@
 import Container from "../Container";
+import Logo from "../Logo";
+import styles from './footer.module.scss';
 
 export default function Footer() {
   return (
-    <Container>Footer</Container>
+    <footer className={styles.main}>
+    <Container>
+      <div className={styles.content}>
+        <div className={styles.footerResume}>
+          <Logo />
+          <div className={styles.textResume}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.  </div>
+
+          <div className={styles.buttons}>
+            <button className={styles.buttonFooter}>Button 1</button>
+            <button className={styles.buttonFooter}>Button 2</button>
+          </div>
+        </div>
+        <div className={styles.footerSection}>2</div>
+        <div className={styles.footerSection}>3</div>
+        <div className={styles.footerSection}>4</div>
+      </div>
+    </Container>
+
+    </footer>
   )
 }
