b1edb91ead6bc63459696554d97a41110c1e23e3
Romario Alves <romariogn10@gmail.com>
2023-01-29 00:33:48 -0300

    Feat(List Pets): add

--- src/pages/pets/index.tsx
+++ src/pages/pets/index.tsx
@@ -1,9 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
 import { useEffect, useState } from 'react';
-import { api } from '../services/api'
+import { api } from '../../services/api';
+import style from "./pet.module.scss";
+
 
 interface IPet{
+  id: string;
   name: string;
   type: string;
   age: number;
@@ -15,7 +16,8 @@ export default function Home() {
 
   const [ pets, setPets ] = useState<IPet[]>([]);
   useEffect(() => {
-    const pets =  api.get('/pets/list').then(response => setPets(response.data))
+    api.get('/pets/list').then(response => setPets(response.data))
+    console.log(pets);
   }, [])
 
 
@@ -24,7 +26,7 @@ export default function Home() {
     <h1>listagem de pets</h1>
     { pets.map(pet => {
       return (
-        <div key={pet.name}>
+        <div key={pet.name} className={style.container}>
         <h3>Nome: {pet.name}</h3>
         <br />
         <h3>Idade: {pet.age}</h3>
