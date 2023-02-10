2dba3ccd905553dc441c7668f581ced09c4a0242
Romario Alves <romariogn10@gmail.com>
2023-02-09 23:11:44 -0300

    Feat(Card pet): add

--- src/pages/pets/index.tsx
+++ src/pages/pets/index.tsx
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
+import Container from '../../components/Container';
 import { api } from '../../services/api';
-import style from "./pet.module.scss";
+import style from "./create/pet.module.scss";
+import Image  from "next/image";
+import panteraImg from "../../../public/pantera.jpg";
 
 
-interface IPet{
+interface IPet {
   id: string;
   name: string;
   type: string;
@@ -14,29 +17,59 @@ interface IPet{
 
 export default function Home() {
 
-  const [ pets, setPets ] = useState<IPet[]>([]);
+  const [pets, setPets] = useState<IPet[]>([]);
   useEffect(() => {
     api.get('/pets/list').then(response => setPets(response.data))
     console.log(pets);
   }, [])
 
-
   return (
-    <>
-    <h1>listagem de pets</h1>
-    { pets.map(pet => {
-      return (
-        <div key={pet.name} className={style.container}>
-        <h3>Nome: {pet.name}</h3>
-        <br />
-        <h3>Idade: {pet.age}</h3>
-        <br />
-        <h3>Type: {pet.type}</h3><br />
-        <h3>Peso: {pet.weight}</h3><br />
+    <Container>
+      <div className={style.content}>
+        <div className={style.contentTitle}>
+          <h1 className={style.title}>Latest on the Property Listing</h1>
         </div>
-      ) 
-    }) }
 
-    </>
+        <div className={style.contentPets}>
+          {pets.map(pet => {
+            return (
+              <div key={pet.id} className={style.cardPet}>
+                <Image src={panteraImg} className={style.petImageProfile} width={250} height={250} alt="test"/>
+                <div className={style.container}>
+                  <h3 className={style.titleCardPet}>{pet.name}</h3>
+                  <h3 className={style.descriptionPetCard}>Idade: {pet.age} anos, Classe: {pet.type}, Sexo: {pet.genre == 'female' ? 'Femêa' : 'Macho'}</h3>
+                  <h3 className={style.descriptionPetCard}>Peso: {pet.type}</h3>
+                </div>
+              </div>
+
+
+            )
+          })}
+        </div>
+
+
+
+        <div className={style.contentTitle}>
+          <h1 className={style.title}>Latest on the Property Dogs</h1>
+        </div>
+
+        <div className={style.contentPets}>
+          {pets.map(pet => {
+            return (
+              <div key={pet.id} className={style.cardPet}>
+                <Image src={panteraImg} className={style.petImageProfile} width={250} height={250} alt="test"/>
+                <div className={style.container}>
+                  <h3 className={style.titleCardPet}>{pet.name}</h3>
+                  <h3 className={style.descriptionPetCard}>Idade: {pet.age} anos, Classe: {pet.type}, Sexo: {pet.genre == 'female' ? 'Femêa' : 'Macho'}</h3>
+                  <h3 className={style.descriptionPetCard}>Peso: {pet.type}</h3>
+                </div>
+              </div>
+            )
+          })}
+        </div>
+
+      </div>
+
+    </Container>
   )
 }
